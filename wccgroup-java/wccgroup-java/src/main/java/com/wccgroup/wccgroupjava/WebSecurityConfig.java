7d6c7e3596a6ee9f4089724d16ba14bcfef6d4e2
kah-khan <hermeslim.ck@gmail.com>
2021-07-06 21:19:40 +0800

    websecurityconfig - added sessions management

--- wccgroup-java/wccgroup-java/src/main/java/com/wccgroup/wccgroupjava/WebSecurityConfig.java
+++ wccgroup-java/wccgroup-java/src/main/java/com/wccgroup/wccgroupjava/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -16,8 +17,13 @@ public class WebSecurityConfig
 	 */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .headers().frameOptions().disable();
-    }
+            http
+             .csrf().disable()
+//             .headers().frameOptions().disable() 
+             .authorizeRequests().anyRequest().authenticated()
+             .and()
+             .httpBasic().and()
+             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        }
+      
 }
